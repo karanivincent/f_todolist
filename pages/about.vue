4c11bf908778681bf7500d710ef91cfc3d100655
karanivincent <trevorvinnie@gmail.com>
2020-11-23 22:55:29 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class=" container">
+    <h1 class=" text-2xl text-gray-600 font-semibold">Hello world</h1>
+    <div class=" ">I am an about page</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
